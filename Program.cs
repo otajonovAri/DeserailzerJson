676bb8a60e6c539f92f2d0ab970d603b124c5c4a
OtajonovAri <otajonovmuhammadali668@gmail.com>
2024-12-02 00:08:43 +0500

    Program.cs File

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace DeserailzerJson
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+			OrderControl control = new OrderControl();
+			bool exit = false;
+
+			while (!exit)
+			{
+				Console.WriteLine("\n--- Order Management System ---");
+				Console.WriteLine("1. Add New Order");
+				Console.WriteLine("2. Display Orders (Serialized JSON)");
+				Console.WriteLine("3. Exit");
+				Console.Write("Choose an option: ");
+				string choice = Console.ReadLine();
+
+				switch (choice)
+				{
+					case "1":
+						control.OrderAdding();
+						break;
+					case "2":
+						control.WriteMemoryStream();
+						break;
+					case "3":
+						exit = true;
+						Console.WriteLine("Exiting... Goodbye!");
+						break;
+					default:
+						Console.WriteLine("Invalid choice. Please try again.");
+						break;
+				}
+			}
+		}
+	}
+}
