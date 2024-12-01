b7d56a1ac75c6e9812b955daf47ed66a169de151
OtajonovAri <otajonovmuhammadali668@gmail.com>
2024-12-02 00:07:01 +0500

    Order cs

---
+++ Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeserailzerJson
+{
+	internal class Order
+	{
+		public int OrderId { get; set; }
+		public string CustomerName { get; set; }
+		public decimal TotalAmount { get; set; }
+		public List<OrderItem> Items { get; set; }
+	}
+}
